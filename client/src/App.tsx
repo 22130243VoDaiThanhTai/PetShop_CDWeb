219aa6125ce889ed01319f244a98357026321ac3
Tan <22130246@st.hcmuaf.edu.vn>
2026-05-20 14:42:54 +0700

    thêm giao diện page đăng nhập (hard code)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
-// Component tạm thời cho Trang Chủ trong khi đợi làm các trang chi tiết
 const HomePlaceholder = () => (
     <div className="container text-center" style={{ minHeight: '50vh', padding: '100px 0' }}>
       <h2 className="text-muted">Nội dung Trang Chủ PetShop sẽ hiển thị tại đây</h2>
@@ -14,24 +15,18 @@ const HomePlaceholder = () => (
 function App() {
   return (
       <Router>
-        {/* 1. Thanh điều hướng Header (Luôn cố định ở trên) */}
         <Header />
 
-        {/* Khoảng cách đệm 90px để nội dung không bị cái Header fixed-top đè lên */}
         <div style={{ marginTop: '95px' }}></div>
 
-        {/* 2. Phần nội dung thay đổi linh hoạt theo từng trang */}
         <main style={{ minHeight: '60vh' }}>
           <Routes>
             <Route path="/" element={<HomePlaceholder />} />
-            {/* Sau này bạn làm thêm trang nào thì thêm Route vào đây, ví dụ:
-          <Route path="/introduce" element={<Introduce />} />
-          <Route path="/cart" element={<Cart />} />
-          */}
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
           </Routes>
         </main>
 
-        {/* 3. Chân trang Footer (Luôn nằm ở dưới cùng) */}
         <Footer />
       </Router>
   );
