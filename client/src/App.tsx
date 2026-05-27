37c7a9f9170654b3bd429e10769ca0a94a695990
22130243VoDaiThanhTai <22130243@st.hcmuaf.edu.vn>
2026-05-27 13:00:52 +0700

    Update: Chỉnh sửa đường dẫn Home và sửa Footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import CartPage from "./pages/CartPage";
@@ -22,7 +23,7 @@ function App() {
 
         <main style={{ minHeight: '60vh' }}>
           <Routes>
-            <Route path="/" element={<HomePlaceholder />} />
+            <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/cart" element={<CartPage />} />
