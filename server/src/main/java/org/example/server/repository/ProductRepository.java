a8d590001d5ab6f58e9ee120f0195da2a5a6a342
22130243VoDaiThanhTai <22130243@st.hcmuaf.edu.vn>
2026-05-27 13:02:19 +0700

    New: Repository cho Product

---
+++ server/src/main/java/org/example/server/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package org.example.server.repository;
+
+import org.example.server.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    // Lấy 4 sản phẩm đầu tiên để làm "Hàng Hot" trên trang chủ
+    List<Product> findTop4ByOrderByIdDesc();
+}
